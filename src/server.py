965807300eaf6e19eee7e16ab002b252f3f85dfe
VavaFisud <vavafisudemailproyt@gmail.com>
2025-11-09 19:42:30 +0100

    refactor CORS middleware integration to use Middleware class

--- src/server.py
+++ src/server.py
@@ -6,6 +6,7 @@ from datetime import date, datetime, timedelta
 from typing import Any, Dict, List
 
 from fastmcp import FastMCP
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import Response
 
@@ -28,11 +29,13 @@ poller = UpdatePoller(settings, service, notifier)
 
 mcp = FastMCP("EcoleDirecte <> Poke")
 mcp.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_credentials=True,
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
 )
 
 
